f33d126f926729a80a30b0ab64c8a2c097027d31
CarlosMJS <94412169+CarlosMJS@users.noreply.github.com>
2023-06-05 22:11:18 -0400

    Add files via upload

---
+++ enunciado 2/workflow/terminoPlanEstudio.inc.php
@@ -0,0 +1,19 @@
+
+
+<div class="card text-center">
+  <div class="card-header">
+    ¿HA CULIMNADO SUS ESTUDIOS ACADEMICOS?
+  </div style=" text-align: center;">
+  <div>
+    <img src="https://cdn-icons-png.flaticon.com/512/2726/2726521.png" style="width: 18rem;" class="mt-5 center-block" alt="...">
+  </div>
+  
+  <div class="card-body">
+  
+    <h5 class="card-title"> ¿HA FINALIZADO EXITOSAMENTE LA CULMINACION DE SUS ESTUDIOS?</h5>
+        
+  </div>
+  <div class="card-footer text-muted">
+    JAHUIRA SULLCA CARLOS MANUEL - LEGUA VILLEGAS JHOEL MAURICIO
+  </div>
+</div>
