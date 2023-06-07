923842d0d6785209921251381894a04e11891f69
CarlosMJS <94412169+CarlosMJS@users.noreply.github.com>
2023-06-07 13:52:18 -0400

    Add files via upload

---
+++ enunciado 2/workflow/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/login.css">
+	<title>INF 324</title>
+</head>
+<body>
+	<div class="form-body">
+	<img src="https://w0.peakpx.com/wallpaper/142/632/HD-wallpaper-male-user-neon-icon-blue-background-neon-symbols-male-user-neon-icons-male-user-sign-computer-signs-male-user-icon-computer-icons.jpg" alt="Descripción de la imagen">
+	</body>
+		<p class = "text">Bienvenido al Sistema</p>
+			<form class="login-form" method="GET" action="indexcontrol.php">
+				<input type="text" placeholder="usuario" id="basic-addon3" name="usuario" required class = "form-control"><br>
+				<input type="password" placeholder="password" id="basic-addon3" name="contrasenia" required><br>
+				<input class="btn btn-success" type="submit" value="INICIAR" name="Aceptar"/>
+			</form>
+		</div>
+		<div class="card" style="width: 38rem;">
+  <div class="row">
+    <div class="col">  
+      <img class="card-img-top" src="img/jhoel.jpeg" alt="Card image 1">
+      <a href="https://wa.me/73282523">LEGUA VILLEGAS JHOEL MAURICIO</a>
+    </div>
+    <div class="col">
+      <img class="card-img-top" src="img/carlos.jpeg" alt="Card image 2">
+      <a href="https://wa.me/62478187">JAHUIRA SULLCA CARLOS MANUEL</a>
+    </div>
+  </div>
+</div>
+
+	</body>
+
+</html>
---
+++ enunciado 2/workflow/solicita.inc.php
@@ -0,0 +1,15 @@
+<div class="card text-center">
+    <div class="card-header">
+        ** ARRANQUE DE PROCESOS **
+    </div>
+    <div class="card text-white bg-dark mb-3" style="max-width: 18rem; margin: 0 auto;">
+        <div class="card-header">************************************</div>
+        <div class="card-body">
+            <h5 class="card-title">SOLICITUD DE CERTIFICADO DE EGRESO</h5>
+            <p class="card-text">Proceso para solicitar su certificado de egreso.</p>
+        </div>
+    </div>
+    <div class="card-footer text-muted">
+        JAHUIRA SULLCA CARLOS MANUEL - LEGUA VILLEGAS JHOEL MAURICIO
+    </div>
+</div>
